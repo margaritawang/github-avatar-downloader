8dd3c30a644def4970e498c73864f7f150af2ee5
Margarita Amy Wang <amywang.2@alumni.ubc.ca>
2017-12-05 02:48:24 +0000

    added step 7

--- download_avatars.js
+++ download_avatars.js
@@ -7,18 +7,26 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url : 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    json: true,
     headers: {
       'User-agent': 'request',
       'Authorization': 'token ' + token.GITHUB_TOKEN
     }
   };
 
-  request(options, function(err, res,body) {
+  request(options, function(err, res, body) {
+  //  var obj = JSON.parse(body);
     cb(err,body);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  if (err) {
+    console.log('Error: ', err);
+  }
+
+  for (i in result) {
+    console.log('avatar_url: ' + result[i]['avatar_url']);
+  }
+
 });
